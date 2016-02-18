7870bb2d190f2bacf4c0a7b557b64214e3a7b217
Carlos Arcenas <c.arcenas@me.com>
2016-02-18 17:53:29 +0800

    Initial Commit

---
+++ Pocket JudgeTests/Pocket_JudgeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pocket_JudgeTests.swift
+//  Pocket JudgeTests
+//
+//  Created by Carlos Arcenas on 2/18/16.
+//  Copyright © 2016 Carlos Arcenas. All rights reserved.
+//
+
+import XCTest
+@testable import Pocket_Judge
+
+class Pocket_JudgeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
